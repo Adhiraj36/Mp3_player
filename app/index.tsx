00db0093eb295c9199b290e218631df9350e5aa5
Adhiraj Deshmukh <adhirajdd20@gmail.com>
2026-03-18 21:58:10 +0530

    added ui

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,81 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-export default function Index() {
+export default function Login() {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (key:any , value:any) => {
+    setForm({ ...form, [key]: value });
+  };
+
+  const handleSubmit = () => {
+    console.log("Login Data:", form);
+    // later → API call
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+
+      <TextInput
+        placeholder="Email"
+        value={form.email}
+        onChangeText={(text: any) => handleChange("email", text)}
+        style={styles.input}
+        keyboardType="email-address"
+      />
+
+      <TextInput
+        placeholder="Password"
+        value={form.password}
+        onChangeText={(text) => handleChange("password", text)}
+        style={styles.input}
+        secureTextEntry
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+    backgroundColor: "#f5f5f5",
+  },
+  title: {
+    fontSize: 28,
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  input: {
+    backgroundColor: "#fff",
+    padding: 12,
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#007bff",
+    padding: 14,
+    borderRadius: 8,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+});
